ced770cf95ec1b2abaab029b83c67663ee20ef45
AloneAlongLife <xd0931021@gmail.com>
2022-12-17 09:28:46 +0800

    Auto Commit

--- main.py
+++ main.py
@@ -1,72 +1,92 @@
 from bs4 import BeautifulSoup, Tag
 from requests import session
-from orjson import loads
-from time import time_ns, sleep
+from orjson import loads, JSONDecodeError
+from time import time, sleep
+from numpy import frombuffer, uint8
+from cv2 import imdecode, IMREAD_COLOR, cvtColor, COLOR_BGR2GRAY, INTER_NEAREST, resize
 
-sen = session()
-
-# URL = "https://tixcraft.com/activity/game/23_ssf4"
+URL = "https://tixcraft.com/activity/game/23_ssf4"
 # URL = "https://tixcraft.com/activity/game/23_wbc"
 URL = "https://tixcraft.com/activity/game/23_goodband"
-TICKS = 1
 
-sen.cookies.set("SID", "48lpsjuko33o9jpt84hh870dai")
+SID = input("SID: ")
+SID = "tr1ofvg0fmjrrsanm8nref8jda"
 
-timer = time_ns()
+try: TICKS = int(input("Ticket Count: "))
+except: TICKS = 1
+
+sen = session()
+sen.cookies.set("SID", SID)
+
+timer = time()
+while not (game_tag := BeautifulSoup((r := sen.get(URL)).content, features="html.parser").select_one("#gameList input")):
+    print(f"\r尚未開賣 {r.status_code} {format(time() - timer, '.2f')}s", end="")
+
+timer = time()
 print("timer_start")
 
-while True:
-    try:
-        r = sen.get(URL)
-        home_page: BeautifulSoup = BeautifulSoup(r.content, features="html.parser")
-        game_url = home_page.select_one("#gameList input")["data-href"]
-        break
-    except:
-        print(f"\r尚未開賣 {r.status_code} {time_ns() - timer}", end="")
-        # sleep(0.05)
-        continue
-
-games: BeautifulSoup = BeautifulSoup(sen.get(f"https://tixcraft.com{game_url}").content, features="html.parser")
+games: BeautifulSoup = BeautifulSoup(sen.get(f"https://tixcraft.com{game_tag['data-href']}").content, features="html.parser")
 url_data = loads(games.decode().split("var areaUrlList = ")[1].split(";")[0].replace("\\", ""))
 
-a_tag = games.select_one("div.zone.area-list a")
-game_id = a_tag["id"]
-print(f"取得: {a_tag.text}")
+while True:
+    a_tag = games.select_one("div.zone.area-list a")
+    game_id = a_tag["id"]
+    print(f"取得: {a_tag.text} t={time() - timer}")
 
-order_url = f"https://tixcraft.com{url_data[game_id]}"
-order_page: BeautifulSoup = BeautifulSoup(sen.get(order_url).content, features="html.parser")
+    order_url = f"https://tixcraft.com{url_data[game_id]}"
+    order_page: BeautifulSoup = BeautifulSoup(sen.get(order_url).content, features="html.parser")
 
-form: Tag = order_page.select_one("form")
-agree = "TicketForm[agree]" + order_page.decode().split('\"name\", \"TicketForm[agree]')[1].split('\");')[0]
+    form: Tag = order_page.select_one("form")
+    agree = "TicketForm[agree]" + order_page.decode().split('\"name\", \"TicketForm[agree]')[1].split('\");')[0]
 
-form_data = {
-    "CSRFTOKEN": form.select_one("#CSRFTOKEN")["value"],
-    agree: 1,
-    "ticketPriceSubmit": ""
-}
+    form_data = {
+        "CSRFTOKEN": form.select_one("#CSRFTOKEN")["value"],
+        agree: 1,
+        "ticketPriceSubmit": ""
+    }
+    tick_list = form.select("#ticketPriceList select")
+    tick_list_2 = form.select("#ticketPriceList input")
+    form_data[tick_list[0]["name"]] = TICKS
+    form_data[tick_list_2[0]["name"]] = tick_list_2[0]["value"]
+    for tic, pic in zip(tick_list[1:], tick_list_2[1:]):
+        form_data[tic["name"]] = 0
+        form_data[pic["name"]] = pic["value"]
 
-tick_list = form.select("#ticketPriceList select")
-tick_list_2 = form.select("#ticketPriceList input")
-form_data[tick_list[0]["name"]] = TICKS
-form_data[tick_list_2[0]["name"]] = tick_list_2[0]["value"]
+    # open("out.png", mode="wb").write(sen.get("https://tixcraft.com/ticket/captcha").content)
+    for _ in range(3):
+        out = []
+        for _ in range(3):
+            img = imdecode(frombuffer(sen.get("https://tixcraft.com/ticket/captcha").content, uint8), IMREAD_COLOR)
+            img = cvtColor(img, COLOR_BGR2GRAY)
+            img = resize(img, (75, 40), interpolation=INTER_NEAREST)
 
-for i, tic in enumerate(tick_list[1:], 1):
-    form_data[tic["name"]] = 0
-    form_data[tick_list_2[i]["name"]] = tick_list_2[i]["value"]
+            img[img < 250] = 0
+            temp = img.astype("U2")
+            temp[temp == "0"] = " "
+            temp[temp != " "] = "#"
+            temp = [l for l in temp.tolist() if "#" in l]
+            out.append(temp)
+        print("\n".join(map(lambda x: "|".join(map(lambda y: "".join(y), x)), zip(out[0], out[1], out[2]))))
+    form_data["TicketForm[verifyCode]"] = input("verifyCode:")
 
-open("out.png", mode="wb").write(sen.get("https://tixcraft.com/ticket/captcha").content)
-form_data["TicketForm[verifyCode]"] = input("verifyCode:")
+    sen.post(order_url, data=form_data)
+    check_res = sen.get("https://tixcraft.com/ticket/check")
 
-sen.post(order_url, data=form_data)
-check_res = sen.get("https://tixcraft.com/ticket/check")
+    try:
+        check_data = loads(check_res.content)
+    except JSONDecodeError:
+        print("SID錯誤 請重新輸入")
+        SID = input("SID: ")
+        sen.cookies.set("SID", SID)
+        continue
+    print(check_data["message"])
+    if check_data["waiting"]:
+        sleep(check_data["time"])
+        print("搶票成功 請前往該帳號訂單查看: https://tixcraft.com/ticket/checkout")
+        break
+    else:
+        print(check_res.content)
+        input("搶票失敗 重新開始...")
+        games: BeautifulSoup = BeautifulSoup(sen.get(f"https://tixcraft.com{game_tag['data-href']}").content, features="html.parser")
 
-check_data = loads(check_res.content)
-print(check_data["message"])
-if check_data["waiting"]:
-    sleep(check_data["time"])
-    print("搶票成功 請前往該帳號訂單查看: https://tixcraft.com/order")
-else:
-    print(check_res.content)
-    input("搶票失敗 按Enter鍵離開...")
-
-print((time_ns() - timer) * 10**-9)
+print((time() - timer))
